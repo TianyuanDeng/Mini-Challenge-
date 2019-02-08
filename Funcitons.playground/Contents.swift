82aea2281a8bdbdb9df09fea4fb54d24e5fe14c7
Tianyuan Deng <wergelesn@missouri.edu>
2019-02-08 17:46:09 -0600

    Add func

---
+++ Funcitons.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func check(value : Int) -> String? {
+    if (value <= 0) {
+        return nil
+    }else {
+        return String(value)
+    }
+}
+
+let numberString: String? = check(value: 222)
